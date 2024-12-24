dd04b42f243f0d15f80ce5c979b7a5fc30001bbe
Nandan Pandey <npandey@egnyte.com>
2024-12-24 20:45:40 +0530

    rate limiter

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoutes from "./routes/auth.routes";
+import { rateLimiter } from "./middlewares/rateLimiter.middleware";
+import logger from "./utils/logger.util";
 
 dotenv.config();
 
@@ -10,6 +12,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+app.use(rateLimiter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
@@ -21,10 +24,12 @@ app.use("/api/auth", authRoutes);
 // Connect to MongoDB
 mongoose
   .connect(process.env.DB_URL!)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error) => console.error("Database connection failed:", error));
+  .then(() => logger.info("Connected to MongoDB"))
+  .catch((error) =>
+    logger.error(`Database connection failed: ${error.message}`)
+  );
 
 // Start Server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  logger.info(`Server is running on http://localhost:${port}`);
 });
